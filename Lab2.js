fa09b420c74ad92c949ba61bf57b026be275e192
TedroySimmons <157929149+TedroySimmons@users.noreply.github.com>
2024-09-16 00:04:45 -0400

    Add files via upload

---
+++ Lab2.js
@@ -0,0 +1,24 @@
+// Exercise 1
+
+// Exercise 2
+
+const capitalize = ([upper, ...lower]) => upper.toUpperCase() + lower.join('');
+console.log(capitalize('foobar'));
+console.log(capitalize('nodejs'));
+
+// Exercise 3
+
+//const colors = ['red', 'green', 'blue']
+//const capitalizeColors = ([upper, ...lower]) => upper.toUpperCase() + lower.join('');
+//const capitalizeColors = colors.map(
+//console.log(capitalizeColors)
+
+// Exercise 4
+//function lessthan(v) {return v <20;}
+//var values = [1, 60, 34, 30, 20, 5].filter(lessthan);
+//console.log(values);
+
+// Exercise 5
+
+// Exercise 6
+
